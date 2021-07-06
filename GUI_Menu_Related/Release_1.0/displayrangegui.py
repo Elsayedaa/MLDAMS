89a74bbf93be43ab4b69702eb240c6473b6197d9
Elsayed <elsayeda@hhmi.org>
2021-07-06 13:51:17 -0400

    edited displayrangegui entry conditions and error messages

--- GUI_Menu_Related/Release_1.0/displayrangegui.py
+++ GUI_Menu_Related/Release_1.0/displayrangegui.py
@@ -1,3 +1,4 @@
+#*
 import sys
 import os
 from pathlib import PurePath
@@ -6,6 +7,7 @@ abspath = str(filepath.parent)
 appParentDir = abspath.replace(r'\GUI_Menu_Related\Release_1.0','')
 sys.path.append(r"{}\GUI_Menu_Related\Release_1.0".format(appParentDir))
 import startpage
+import re
 import pyautogui
 import json
 from tkinter import *
@@ -51,14 +53,14 @@ class displayRangeTree(Frame):
         max_label = Label(self.mainframe, text = "Display maximum:")
         max_label.grid(row = 1, column = 2)
 
-        sample_variable = StringVar()
-        self.sample_entry = ttk.Entry(self.mainframe, textvariable = sample_variable)
+        self.sample_variable = StringVar()
+        self.sample_entry = ttk.Entry(self.mainframe, textvariable = self.sample_variable)
         self.sample_entry.grid(row = 2, column = 0, sticky = "ew", pady = 5)
-        min_variable = StringVar()
-        self.min_entry = ttk.Entry(self.mainframe, textvariable = min_variable)
+        self.min_variable = StringVar()
+        self.min_entry = ttk.Entry(self.mainframe, textvariable = self.min_variable)
         self.min_entry.grid(row = 2, column = 1, sticky = "ew", pady = 5)
-        max_variable = StringVar()
-        self.max_entry = ttk.Entry(self.mainframe, textvariable = max_variable)
+        self.max_variable = StringVar()
+        self.max_entry = ttk.Entry(self.mainframe, textvariable = self.max_variable)
         self.max_entry.grid(row = 2, column = 2, sticky = "ew", pady = 5)
 
         add_new = ttk.Button(self.mainframe, text = "Add new row", command = self.add_row)
@@ -72,6 +74,22 @@ class displayRangeTree(Frame):
         exit_button.grid(row = 4, column = 1, sticky = "ew")
     
     def add_row(self):
+        #define entry conditions
+        if re.search(r"\d{4}-\d{2}-\d{2}", self.sample_entry.get()) == None:
+            self.sample_variable.set("Invalid entry, please enter in YYYY-MM-DD format.")
+        if re.search(r"^\d{5}$", self.min_entry.get()) == None:
+            self.min_variable.set("Invalid entry, please enter 5 digit number")
+        if re.search(r"^\d{5}$", self.max_entry.get()) == None:
+            self.max_variable.set("Invalid entry, please enter 5 digit number")
+
+        #if any of the entry conditions aren't met, a new row won't be made
+        if (
+            re.search(r"\d{4}-\d{2}-\d{2}", self.sample_entry.get()) == None or
+            re.search(r"^\d{5}$", self.min_entry.get()) == None or
+            re.search(r"^\d{5}$", self.max_entry.get()) == None
+        ):
+            return
+        
         #add new row
         id = list(self.treeindex.keys())[-1]+1
         self.treeindex[id] = self.sample_entry.get()
@@ -84,7 +102,7 @@ class displayRangeTree(Frame):
         self.diplay_settings["Samples"].append(
             {
                 "SampleStr": self.sample_entry.get(),
-                "DisplayRange": [self.min_entry.get(), self.max_entry.get()]
+                "DisplayRange": [int(self.min_entry.get()), int(self.max_entry.get())]
             }
         )
 
@@ -95,45 +113,94 @@ class displayRangeTree(Frame):
 
     def update_row(self):
 
-        #update selected row
-        if self.sample_entry.get() == "":
-            sample_update = self.reviewTree.item(self.clickedindex)['text']
+        #retrieve selected row
+        try:
+            row_id = self.reviewTree.selection()[0]
+        except IndexError:
+            none_selected_warning = Toplevel()
+
+            screen_width = self.controller.winfo_screenwidth()
+            screen_height = self.controller.winfo_screenheight()
+
+            window_height = 70
+            window_width = 175
+
+            x_cordinate = int((screen_width/2) - (window_width/2))
+            y_cordinate = int((screen_height/2) - (window_height/2))
+
+            none_selected_warning.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
+            warninglabel = Label(none_selected_warning, text = "Error:\nNo row was selected.")
+            warninglabel.pack()
+            okbutton = ttk.Button(none_selected_warning, text = "Ok", command = none_selected_warning.destroy)
+            okbutton.pack()
+            return
+
+        #update variable values are changed for valid entries; maintain current record value if entry is invalid
+        if re.search(r"\d{4}-\d{2}-\d{2}", self.sample_entry.get()) == None:
+            self.sample_variable.set("Invalid entry, please enter in YYYY-MM-DD format.")
+            sample_update = self.reviewTree.item(row_id)['text']
         else:
             sample_update = self.sample_entry.get()
         
-        if self.min_entry.get() == "":
-            min_update = self.reviewTree.set(self.clickedindex, "#1")
+        if re.search(r"^\d{5}$", self.min_entry.get()) == None:
+            self.min_variable.set("Invalid entry, please enter 5 digit number")
+            min_update = self.reviewTree.set(row_id, "#1")
         else:
             min_update = self.min_entry.get()
 
-        if self.max_entry.get() == "":
-            max_update = self.reviewTree.set(self.clickedindex, "#2")
+        if re.search(r"^\d{5}$", self.max_entry.get()) == None:
+            self.max_variable.set("Invalid entry, please enter 5 digit number")
+            max_update = self.reviewTree.set(row_id, "#2")
         else:
             max_update = self.max_entry.get()
         
-        self.reviewTree.delete(self.clickedindex)
-
-        self.reviewTree.insert(parent='', index=self.clickedindex, iid = self.clickedindex, text = sample_update, values = (
+        #row is deleted and replaced with one that has the updates
+        self.reviewTree.delete(row_id)
+        self.reviewTree.insert(parent='', index=row_id, iid = row_id, text = sample_update, values = (
             min_update, 
             max_update
         ))
 
-        #update selection in display settings json
-        for dic in self.diplay_settings["Samples"]:
-            if dic["SampleStr"] == self.reviewTree.item(self.clickedindex)['text']:
-                dic["SampleStr"] = sample_update
-                dic["DisplayRange"] = [min_update, max_update]
-        
+        #update selection in display settings json        
+        updatedic = self.diplay_settings["Samples"][int(row_id)]
+        updatedic["SampleStr"] = sample_update
+        updatedic["DisplayRange"] = [int(min_update), int(max_update)]
+
         #save display settings json
         write = write = json.dumps(self.diplay_settings, indent = 4)
         with open(r"\\dm11\mousebrainmicro\registration\Database\displaySettings.json", "w") as f:
             f.write(write)   
 
     def delete_row(self):
-        selected_sample = self.reviewTree.item(self.clickedindex)['text']
-        self.reviewTree.delete(self.clickedindex)
+        #retrieve selected row
+        try:
+            row_id = self.reviewTree.selection()[0]
+        except IndexError:
+            none_selected_warning = Toplevel()
+
+            screen_width = self.controller.winfo_screenwidth()
+            screen_height = self.controller.winfo_screenheight()
+
+            window_height = 70
+            window_width = 175
+
+            x_cordinate = int((screen_width/2) - (window_width/2))
+            y_cordinate = int((screen_height/2) - (window_height/2))
+
+            none_selected_warning.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
+            warninglabel = Label(none_selected_warning, text = "Error:\nNo row was selected.")
+            warninglabel.pack()
+            okbutton = ttk.Button(none_selected_warning, text = "Ok", command = none_selected_warning.destroy)
+            okbutton.pack()
+            return
+
+        #delete selected row and update index of proceeding rows
+        selected_sample = self.reviewTree.item(row_id)['text']
+        self.reviewTree.delete(row_id)
         for i in self.reviewTree.get_children():
-            if int(i) > int(self.clickedindex)-1:
+            if int(i) > int(row_id)-1:
                 sample = self.reviewTree.item(i)['text']
                 disp_min = self.reviewTree.set(i, "#1")
                 disp_max = self.reviewTree.set(i, "#2")
@@ -170,15 +237,8 @@ class displayRangeTree(Frame):
             self.copied_msg = Toplevel()
             self.copied_msg.wm_overrideredirect(True) 
 
-            screen_width = self.controller.winfo_screenwidth()
-            screen_height = self.controller.winfo_screenheight()
-
             window_height = 20
             window_width = 75
-
-            x_cordinate = int((screen_width/2) - (window_width/2))
-            y_cordinate = int((screen_height/2) - (window_height/2))
-            
             pos_x, pos_y = pyautogui.position()
 
             self.copied_msg.geometry(f"{window_width}x{window_height}+{pos_x}+{pos_y}")
